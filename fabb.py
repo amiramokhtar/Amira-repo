1203d3bbced7b7c7ea09cdb41cf1c501d561c13a
amiramokhtar <159718564+amiramokhtar@users.noreply.github.com>
2024-12-02 19:14:08 +0200

    Add files via upload

---
+++ fabb.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import plotly.express as px  # Correcting the typo in "plotly.express"
+
+# Correcting "set.set_pege_config" to "st.set_page_config"
+st.set_page_config(
+    layout="wide",
+    page_title="Simple Dashboard"  # Fixed the typo in "simple Dashboard"
+)
+
+st.title("Simple Dashboard")
+# Add your Streamlit components here for visualization
